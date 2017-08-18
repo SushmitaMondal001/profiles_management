612dae42549205fbbed1ca22fac5d007b517bc0e
Van Cam Huynh <vancamhuynh@192-168-1-5.tpgi.com.au>
2017-08-19 02:09:41 +1000

    test

--- public/js/controllers/AddCtrl.js
+++ public/js/controllers/AddCtrl.js
@@ -1,1 +1,15 @@
-angular.module('AddCtrl',[]).controller('AddController', function($scope){});
+angular.module('AddCtrl',['ProfileService']).controller('AddController', 'Profile', function(Profile){
+  var vm = this;
+
+  vm.add = function () {
+    console.log('add function call!!!!!');
+    Profile.create(vm.change).then(function (response) {
+    //vm.movie = Object.assign({}, vm.change);
+    console.log('response status: ' + response.status);
+    console.log('status text: ' + response.statusText);
+  });
+  };
+
+
+
+});
